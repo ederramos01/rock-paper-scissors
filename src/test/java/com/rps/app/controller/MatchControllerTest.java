a437c79e86309c123550d4de31b4e3f3b83ae500
ederramos01 <eder.ramos01@telusinternational.com>
2023-03-03 16:12:07 -0600

    MatchControllerTest modified

--- src/test/java/com/rps/app/controller/MatchControllerTest.java
+++ src/test/java/com/rps/app/controller/MatchControllerTest.java
@@ -2,9 +2,15 @@ package com.rps.app.controller;
 
 import org.junit.jupiter.api.Test;
 
+import com.rps.app.model.player.PlayerModel;
+
 public class MatchControllerTest {
     @Test
     void testAnnounceWinnerOfTheMatch() {
+
+        PlayerModel playerModel = new PlayerModel("Player1");
+
+
 
     }
 }
