eabd887820b8bd6c83d3daa70e61c2b798747260
ederramos01 <eder.ramos01@telusinternational.com>
2023-03-03 10:11:10 -0600

    <dependency>

--- src/test/java/com/rps/app/view/MatchViewTest.java
+++ src/test/java/com/rps/app/view/MatchViewTest.java
@@ -1,8 +1,5 @@
 package com.rps.app.view;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
