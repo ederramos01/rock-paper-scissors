34328e705d4d50e9c19dfbed02cafd1d3682a34c
Daniel Lopez <daniel.lopez09@telusinternational.com>
2023-03-05 15:02:28 -0600

    PlayerFactory added

--- src/main/java/com/rps/app/controller/MatchController.java
+++ src/main/java/com/rps/app/controller/MatchController.java
@@ -3,6 +3,7 @@ package com.rps.app.controller;
 import com.rps.app.controller.player.ComputerPlayerController;
 import com.rps.app.controller.player.HumanPlayerController;
 import com.rps.app.controller.player.PlayerController;
+import com.rps.app.controller.player.playersFactory.PlayerFactory;
 import com.rps.app.model.MatchModel;
 import com.rps.app.model.player.PlayerModel;
 import com.rps.app.model.player.PlayerType;
@@ -24,25 +25,10 @@ public class MatchController {
     }
 
     public void setupGame() {
-        //devuelve un typePlayer
+        
         PlayerType[] playersList = matchview.displayMatchMenu();
-
-        // switch(flagOption) {
-        //     case 1 -> {
-        //         System.out.println("option received " + flagOption);
-        //         playersController[0] = new HumanPlayerController();
-        //         playersController[1] = new HumanPlayerController();
-        //     }
-        //     case 2 -> {
-        //         playersController[0] = new HumanPlayerController();
-        //         playersController[1] = new ComputerPlayerController();
-        //     }
-        //     case 3 -> {
-        //         playersController[0] = new ComputerPlayerController();
-        //         playersController[1] = new ComputerPlayerController();
-        //     }
-        // }
-
+        PlayerFactory.CreatePlayer(playersController, playersList);
+        // hacer estas dos lineas de forma automatica en un metodo
         playersController[0].getNewGamePlayer();
         playersController[1].getNewGamePlayer();
         matchModel.newMatch(new PlayerModel[]{playersController[0].getPlayer(), playersController[1].getPlayer()});
